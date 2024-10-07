a9a945363cf2c519862c99290b9e0b44aaefad5a
Thang0226 <thang.nd0226@gmail.com>
2024-10-07 11:31:43 +0700

    Prob3 add requirements for book id and book year

--- Problem3/addWindow.js
+++ Problem3/addWindow.js
@@ -4,6 +4,16 @@ function submitBook() {
   let year = document.getElementById("year").value;
   let count = document.getElementById("count").value;
 
+  if (!+id || id.length > 5 || id[0] > 3 || id[0] < 1) {
+    document.getElementById("id").value = null;
+    return;
+  }
+
+  if (year > 10000) {
+    document.getElementById("year").value = null;
+    return;
+  }
+
   // Access the opener window and call the addNewBook function and display book list again
   if (window.opener && !window.opener.closed) {
     window.opener.addNewBook(id, name, year, count);
